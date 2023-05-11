b996e3a08353af3abdb1e0c202d66ced28c50d49
Jihwan <kjh26720@snu.ac.kr>
2023-05-11 23:30:20 +0900

    modify git ignore

--- core/map_initialization.py
+++ core/map_initialization.py
@@ -3,5 +3,12 @@ import numpy as np
 import os
 
 
-def load_images() :
+def load_images(path1="./data/img1.png", path2="./data/img2.png") :
+    img1 = cv.imread(path1)
+    img2 = cv.imread(path2)
+    
+    print(img1.shape)
     return
+
+
+load_images()
